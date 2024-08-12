4e6571d13a73e08509fba6f12503ceb10f4bf553
20btcs037hy <115235361+20btcs037hy@users.noreply.github.com>
2024-08-13 01:18:20 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,15 @@
+// Task 1
+package driver;
+
+public class Main {
+    
+    public static void main(String s[]){
+        // Task 3
+        RWOnly obj = new RWOnly();
+        // Task 4
+        // obj.name = "AquibEzaz";
+        obj.setName("AquibEzaz");
+
+        System.out.println("Here i am "+obj.getName());
+    }
+}
---
+++ RWOnly.java
@@ -0,0 +1,14 @@
+// Task 1
+package driver;
+// Task 2
+class RWOnly{
+    private String name;
+
+    private String getName(){
+        return name;
+    }
+    private void setName(String name){
+        this.name = name;
+    }
+
+}
