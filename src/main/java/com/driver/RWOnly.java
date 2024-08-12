5918d4854686f18db03cf582bf422d19dc6922b8
20btcs037hy <115235361+20btcs037hy@users.noreply.github.com>
2024-08-12 19:10:44 +0530

    Update RWOnly.java

--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -2,8 +2,8 @@ package com.driver;
 
 public class RWOnly {
     // private member variable name
-    private String str="AquibEzaz";
-
-    Systyem.out.println(str);
-    
+    private String str;
+    void setting(String str){
+        Systyem.out.println(str);
+    }
 }
