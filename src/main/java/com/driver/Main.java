94394879fecf6cd5e8c2a2afd3d95886a00acd94
20btcs037hy <115235361+20btcs037hy@users.noreply.github.com>
2024-08-13 01:16:50 +0530

    Update Main.java

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,8 +1,15 @@
 package com.driver;
 
+// Task 1
 public class Main {
-  // 
-  public static void main(String s[]){
-    RWOnly rw=new RWOnly();
-  
+    
+    public static void main(String s[]){
+        // Task 3
+        RWOnly obj = new RWOnly();
+        // Task 4
+        // obj.name = "AquibEzaz";
+        obj.setName("AquibEzaz");
+
+        System.out.println("Here i am "+obj.getName());
+    }
 }
